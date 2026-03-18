f28e9a4eb92cf3280055e111d3690429e3455579
Magda Chikhradze <magda292005@gmail.com>
2026-03-18 14:04:52 +0400

    indexOf

---
+++ main.js
@@ -0,0 +1,21 @@
+// let lesson = 7;
+// let sentence = "-how many lessons do we have today? -we have - ";
+
+// console.log(sentence + lesson);
+
+// hello = "java";
+
+// let arr = [1234, hello, sentence, lesson, "java", [], {}];
+
+// let player = {
+//   height: 1.7,
+//   weight: 50,
+//   origin: "mars",
+// };
+
+// let isAdult = false;
+// let user = null;
+// let item;
+
+let email = "chikhradzemagda@itvet.com";
+console.log(email.indexOf("@"));
